b2b0aa144318b773c4ea6e3447ab1aec6c3a6c1b
Peter elmered <peter.elmered@angrycreative.se>
2017-02-28 15:27:56 +0100

    Process multiple filters

--- src/Traits/APIModel.php
+++ src/Traits/APIModel.php
@@ -30,6 +30,7 @@ trait APIModel
 
     public function scopeProcessFilters($query, $filters)
     {
+
         if (empty($filters))
         {
             return $query;
@@ -41,14 +42,27 @@ trait APIModel
                 $fields = explode('.', $filter['field']);
 
                 $query->whereHas($fields[0], function ($query) use($fields, $filter) {
-                    $query->where($fields[1], $filter['operator'], $filter['value']);
+                    $query->where('custom_meta.'.$fields[1], $filter['operator'], $filter['value']);
                 });
             }
             else
             {
-                $query->where($filter['field'], $filter['operator'], $filter['value']);
+                if( is_array( $filter['value'] ) )
+                {
+                    $query->whereHas($filter['field'], function ($query) use ($filter) {
+                        foreach($filter['value'] as $key => $value)
+                        {
+                            $query->where($key, 'like', '%'.$value.'%');
+                        }
+                    })->get();
+                }
+                else
+                {
+                    $query->where($filter['field'], $filter['operator'], $filter['value']);
+                }
             }
         }
+
         return $query;
     }
 }
